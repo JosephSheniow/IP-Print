63a84b89e3e49fd999bf390e029c4345e90542e0
JosephSheniow <mrjjjjjoseph@gmail.com>
2018-04-18 00:26:01 +0800

    first commit

---
+++ IP_print.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/get_ip', methods=['GET'])
+
+def get_ip():
+	
+	return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+
+if __name__ =='__main__':
+
+	app.run(host='0.0.0.0',port=8000)
