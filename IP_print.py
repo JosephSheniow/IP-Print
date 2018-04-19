a2308198a4cf538712f831310ee3c8c8acdce49b
JosephSheniow <mrjjjjjoseph@gmail.com>
2018-04-19 19:47:52 +0800

    tab change

--- IP_print.py
+++ IP_print.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/get_ip', methods=['GET'])
 
 def get_ip():
-	
-	return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+
+    return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
 
 if __name__ =='__main__':
 
-	app.run(host='0.0.0.0',port=8000)
+    app.run(host='0.0.0.0',port=8000)
